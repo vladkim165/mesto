139c7a0aa2dca600e5dc5b679521bada365c8af3
Vladislav Kim <vladkim165@gmail.com>
2021-06-17 00:45:17 +0300

    1 итерация

---
+++ script.js
@@ -0,0 +1,40 @@
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popupNameElement = popupElement.querySelector('.popup__field_name');
+const popupBioElement = popupElement.querySelector('.popup__field_bio');
+const userName = document.querySelector('.profile__title');
+const userBio = document.querySelector('.profile__subtitle');
+
+const openPopup = function() {
+  popupElement.classList.add('popup_is-opened');
+  popupNameElement.value = userName.textContent;
+  popupBioElement.value = userBio.textContent;
+}
+
+const closePopup = function() {
+  popupElement.classList.remove('popup_is-opened');
+}
+
+const closePopupByClickOnOverlay = function(event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+
+  closePopup()
+}
+
+const popupSubmitHandler = function (event) {
+  event.preventDefault();
+
+  userName.textContent = popupNameElement.value;
+  userBio.textContent = popupBioElement.value;
+
+  closePopup();
+}
+
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
+popupElement.addEventListener('submit', popupSubmitHandler);
