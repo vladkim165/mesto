5744e483ecc520c97b2808e03efcbfd516f60679
Vladislav Kim <vladkim165@gmail.com>
2021-06-19 01:14:56 +0300

    правки 1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const formOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupElement = document.querySelector('.popup');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-const popupNameElement = popupElement.querySelector('.popup__field_name');
-const popupBioElement = popupElement.querySelector('.popup__field_bio');
+const formCloseButtonElement = popupElement.querySelector('.form__close-button');
+const formNameElement = popupElement.querySelector('.form__field_input_name');
+const formBioElement = popupElement.querySelector('.form__field_input_bio');
 const userName = document.querySelector('.profile__title');
 const userBio = document.querySelector('.profile__subtitle');
 
 const openPopup = function() {
   popupElement.classList.add('popup_opened');
-  popupNameElement.value = userName.textContent;
-  popupBioElement.value = userBio.textContent;
+  formNameElement.value = userName.textContent;
+  formBioElement.value = userBio.textContent;
 }
 
 const closePopup = function() {
@@ -24,17 +24,25 @@ const closePopupByClickOnOverlay = function(event) {
   closePopup()
 }
 
-const popupSubmitHandler = function (event) {
+//функции, открывающие и закрывающие(по клику на крестик и оверлей) попап
+
+
+const editProfileInfo = function (event) {
   event.preventDefault();
 
-  userName.textContent = popupNameElement.value;
-  userBio.textContent = popupBioElement.value;
+  userName.textContent = formNameElement.value;
+  userBio.textContent = formBioElement.value;
 
   closePopup();
 }
 
+//функция, меняющая данные на странице после сабмита в попапе
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
+
+formOpenButtonElement.addEventListener('click', openPopup);
+formCloseButtonElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOnOverlay);
-popupElement.addEventListener('submit', popupSubmitHandler);
+popupElement.addEventListener('submit', editProfileInfo);
+
+
+//добавлены слушатели событий на кнопки
