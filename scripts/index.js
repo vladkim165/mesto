ce97b88b077d6098bf2d01cc5504a84cc94a8ca9
Vladislav Kim <vladkim165@gmail.com>
2021-07-03 16:13:22 +0300

    первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -98,8 +98,8 @@ renderCards(initialCards);
 // Добавили функцию рендера карточек, отрендерили объекты из массива
 
 const addFormButton = document.querySelector('.profile__add-button');
-const addFormPopup = document.querySelector('.popup_add-form');
-const closeAddFormButton = addFormPopup.querySelector('.form__close-button_s');
+const addFormPopup = document.querySelector('.popup_open_add-form');
+const closeAddFormButton = addFormPopup.querySelector('.form__close-button');
 const formPlaceNameElement = addFormPopup.querySelector('.form__field_input_place-name');
 const formLinkElement = addFormPopup.querySelector('.form__field_input_link');
 const cardName = document.querySelector('.element__title');
@@ -133,9 +133,11 @@ const addNewCard = function (event) {
   const cardTemplateContent = document.querySelector('.card-template').content;
   const cardElement = cardTemplateContent.cloneNode(true);
 
+
   cardElement.querySelector('.element__image').src = formLinkElement.value
   cardElement.querySelector('.element__image').alt = formPlaceNameElement.value
   cardElement.querySelector('.element__title').textContent = formPlaceNameElement.value
+
 
   setEventListeners(cardElement);
 
@@ -148,13 +150,57 @@ const addNewCard = function (event) {
 
 addFormPopup.addEventListener('submit', addNewCard);
 
+//обьявим функции при клике на кнопки карточек
 
-//обьявим функцию обработчика событий при клике на кнопки карточек
-function setEventListeners(cardElement) {
-  cardElement.querySelector('.element__like-button').addEventListener('click', handleLikeCard)
-}
-//объявим функции-действия при срабатывании клика на определённую кнопку
 function handleLikeCard(event) {
   event.target.closest('.element').querySelector('.element__like-button').classList.toggle('element__like-button_active')
   console.log('like');
 }
+
+
+
+function handleDeleteCard(event) {
+  event.target.closest('.element').remove();
+}
+
+function openImagePopup(event) {
+  const cardImage = event.target.closest('.element').querySelector('.element__image');
+  const cardTitle = event.target.closest('.element').querySelector('.element__title');
+  popupBackgroundImage.src = cardImage.src;
+  popupBackgroundImage.alt = cardImage.alt;
+  popupTitle.textContent = cardTitle.textContent;
+
+  imagePopup.classList.add('popup_opened');
+}
+
+//обьявим функцию обработчика событий при клике на кнопки карточек
+function setEventListeners(cardElement) {
+  cardElement.querySelector('.element__like-button').addEventListener('click', handleLikeCard);
+  cardElement.querySelector('.element__delete-button').addEventListener('click', handleDeleteCard);
+  cardElement.querySelector('.element__image').addEventListener('click', openImagePopup);
+}
+//объявим функции-действия при срабатывании клика на определённую кнопку
+
+
+const imagePopup = document.querySelector('.popup_open_background-image');
+const popupBackgroundImage = document.querySelector('.popup__background-image');
+const popupTitle = document.querySelector('.popup__image-title');
+
+
+
+function closeImagePopup() {
+  imagePopup.classList.remove('popup_opened');
+}
+
+const closeImagePopupByClickOnOverlay = function (event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+
+  closeImagePopup()
+}
+
+imagePopup.querySelector('.popup__close-button').addEventListener('click', closeImagePopup);
+imagePopup.addEventListener('click', closeImagePopupByClickOnOverlay);
+
+
